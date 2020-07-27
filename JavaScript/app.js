d392ea6781195c059d58cad9952496246a25df85
unknown <bhupendrasingh.ec18@gmail.com>
2020-07-27 23:36:59 +0530

    After refresh page it will show

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -6,30 +6,32 @@ var input = document.getElementById("name");
 let itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
 console.log("let array", itemsArray);
 
+localStorage.setItem('items', JSON.stringify(itemsArray));
+const data = JSON.parse(localStorage.getItem('items'));
+
+
+const liMaker = (text) => {
+    const li = document.createElement('li');
+    li.textContent = text;
+    ul.appendChild(li);
+}
 
 form.addEventListener('submit', function (e) {
     e.preventDefault();
 
     itemsArray.push(input.value);
 
-    const liMaker = (text) => {
-        const li = document.createElement('li');
-        li.textContent = text;
-        ul.appendChild(li);
-    }
-
     localStorage.setItem('items', JSON.stringify(itemsArray));
     liMaker(input.value);
     input.value = "";
 
-    // const data = JSON.parse(localStorage.getItem('items')) || itemsArray;
     // console.log("data",data);
 
-    // itemsArray.forEach(name => {
-    //     console.log("name", name);
-    //     liMaker(name);
-    // });
+});
 
+data.forEach(name => {
+    console.log("name", name);
+    liMaker(name);
 });
 
 button.addEventListener('click', function (e) {
